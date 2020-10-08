35b185473a38f00254271ba7c00902985fb17e2f
CasperCBroeren <c.broeren@gmail.com>
2020-10-08 09:02:17 +0200

    Clean up of AppDataStore

--- src/Models/AppDataStore.ts
+++ src/Models/AppDataStore.ts
@@ -1,5 +1,4 @@
 import { get, Readable, writable } from 'svelte/store' 
-import axios from 'axios';
 import { AppData,  InOfficeAvailable,  User } from './Models';
 import type { AppNavigationType } from './Types';
 
@@ -18,28 +17,25 @@ function createAppData()
         async loadServerData()
         {
             let response = await fetch('https://api.npoint.io/699628d06ae01739b275');            
-            let responseData = await response.json();
-            // this is odd as hell
-            console.log(responseData.officeAvailability[0].date + ' = ' + typeof( responseData.officeAvailability[0].date));
-          
-            var newData  =  new AppData(responseData.user, 
+            let responseData = await response.json();          
+            let newData  =  new AppData(responseData.user, 
                                     responseData.officeAvailability.map(x => new InOfficeAvailable(new Date(x.date), x.persons))); 
             this.set(newData);            
         },
         removeAvailability(user: User, date: Date) {
-            var data = get<AppData, Readable<AppData>>(store);
+            let data = get<AppData, Readable<AppData>>(store);
             const newAvailability = data.officeAvailability.find(x => x.date == date).persons.filter(x => x != user.name);
             data.officeAvailability.find(x => x.date == date).persons = newAvailability; 
             this.set(data);
         },    
         setAvailability(user: User, date: Date) {  
-            var data = get<AppData, Readable<AppData>>(store);
+            let data = get<AppData, Readable<AppData>>(store);
             const newAvailability = [ ... data.officeAvailability.find(x => x.date == date).persons, user.name]; 
             data.officeAvailability.find(x => x.date == date).persons = newAvailability; 
             this.set(data);
         },    
         setCurrentDayIndex(navigation: AppNavigationType) { 
-            var data = get<AppData, Readable<AppData>>(store);
+            let data = get<AppData, Readable<AppData>>(store);
             switch (navigation)
             {
                 case "prev":
